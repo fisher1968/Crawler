ab7ae39adf5366ed04f54874b6158726c9bf07e5
fisher.huang <fisher.huangziqun@gmail.com>
2021-07-24 21:56:20 +0800

    2

--- import requests.py
+++ import requests.py
@@ -5,6 +5,7 @@ import urllib.parse
 import xlwt
 import xlrd
 
+
 url='https://accounts.douban.com/j/mobile/login/basic'
 header={'user-agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
 'Referer': 'https://accounts.douban.com/passport/login_popup?login_source=anony',
