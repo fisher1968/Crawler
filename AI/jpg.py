e33954202666e2814aceb580fdeb46d901cc3c17
fisher.huang PC <fisher.huangziqun@gmail.com>
2021-08-02 22:45:10 +0800

    add char10.py

--- AI/jpg.py
+++ AI/jpg.py
@@ -5,44 +5,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 from imgaug import augmenters as iaa
 import imgaug as ia
+import pickle
 
 # np.set_printoptions(threshold=np.inf)
 
+def print_img(img):
+    print('分辨率',img.shape[0],img.shape[1])
+    print('图片信息',img.shape)
+    plt.imshow(img)
+    plt.show()
+
 img=cv2.imread(".\img\wenyao.jpg")
 
-print('分辨率',img.shape[0],img.shape[1])
-print('图片信息',img.shape)
-plt.imshow(img)
-plt.show()
+print_img(img)
 
 # 读取灰度图
 
 gray_img=cv2.imread(".\img\wenyao.jpg",cv2.IMREAD_GRAYSCALE)
-
-print('灰度图分辨率',gray_img.shape[0],gray_img.shape[1])
-print('灰度图图片信息',gray_img.shape)
-plt.imshow(gray_img)
-plt.show()
+print_img(gray_img)
 
 # 图片resize
 img_400_300=cv2.resize(img,(400,300))
 
-print('reszie图分辨率',img_400_300.shape[0],img_400_300.shape[1])
-print('resize图图片信息',img_400_300.shape)
-plt.imshow(img_400_300)
-plt.show()
+print_img(img_400_300)
 
 
 # 放射变换,放大1.2倍，然后平移
 crop_m_image=np.array([[1.2,0,-50],[0,1.2,-100]],dtype=np.float32)
 crop_img=cv2.warpAffine(img, crop_m_image, (300,200))
-
-print('reszie图分辨率',crop_img.shape[0],crop_img.shape[1])
-print('resize图图片信息',crop_img.shape)
-plt.imshow(crop_img)
-plt.show()
-
-
+print_img(crop_img)
 
 # cv2.imwrite(r".\img\wenyao_crop.jpg",crop_img)
 # cv2.imwrite(r".\img\wenyao_resize.jpg",img_400_300)
@@ -52,19 +43,19 @@ plt.show()
 
 img_girl_all = []
 
-for filename in os.listdir(".\girl"):
-    print(filename)
-    print(len(img_girl_all))    
-    img_girl = cv2.imread(".\girl"+"/"+filename)
-    img_girl_all = img_girl_all.append(img_girl)
-    print(len(img_girl_all))    
+# for filename in os.listdir(".\girl"):
+#     print(filename)
+#     print(len(img_girl_all))    
+#     img_girl = cv2.imread(".\girl"+"/"+filename)
+#     img_girl_all = img_girl_all.append(img_girl)
+#     print(len(img_girl_all))    
 
-plt.show()
+# plt.show()
 
 
-# img=cv2.imread(r".\girl\722ff2ffjw1ejlpyzzzu4j20rs15o18z.jpg")
+img=cv2.imread(r".\girl\722ff2ffjw1ejlpyzzzu4j20rs15o18z.jpg")
 
-# imgs=[img,img,img,img]
+imgs=[img,img,img,img]
 
 seq = iaa.Sequential([         #建立一个名为seq的实例，定义增强方法，用于增强
     iaa.Crop(px=(0, 30)),     #对图像进行crop操作，随机在距离边缘的0到160像素中选择crop范围
@@ -74,11 +65,15 @@ seq = iaa.Sequential([         #建立一个名为seq的实例，定义增强方
     iaa.Affine(rotate=(-25, 25))
 ])
 
-images_aug = seq.augment_images(img_girl_all)
-
+images_aug = seq.augment_images(imgs)
 ia.imshow(np.hstack(images_aug))
 
-# print('reszie图分辨率',imgs.shape[0],img_girl_all.shape[1])
-# print('resize图图片信息',imgs.shape)
-# plt.imshow(images_aug)
-# plt.show()
+def unpickle(file):
+    
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+        return dict 
+
+a=unpickle(".\cifar-10-batches-py")
+
+print(a)
